de61819227b63059c4386ae87db91484c15c32a9
sheddie6 <eddie.sh@gmail.com>
2015-01-19 17:03:28 +0200

    HTTP Server example

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,31 @@
 package main
 
-import ("fmt"
-	"github.com/sheddie6/hello/stringutil"
+import (
+	"io"
+	"net/http"
 )
 
+func hello(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"application/json",
+	)
+	io.WriteString(
+		res,
+		`{"menu": {
+  "id": "file",
+  "value": "File",
+  "popup": {
+    "menuitem": [
+      {"value": "New", "onclick": "CreateNewDoc()"},
+      {"value": "Open", "onclick": "OpenDoc()"},
+      {"value": "Close", "onclick": "CloseDoc()"}
+    ]
+  }
+}}`,
+	)
+}
 func main() {
-    fmt.Printf(stringutil.Reverse("!oG , olleH"))
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":9000", nil)
 }
